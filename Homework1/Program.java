3d67d8172bf6a711998ae89b555b0a74994a57c4
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-04 19:55:03 +0300

    Program file is added

---
+++ Homework1/Program.java
@@ -0,0 +1,9 @@
+package Homework1;
+
+import Homework1.Product;
+
+public class Program {
+    public static void getProductInfo(Product product) {
+        System.out.println(product.toString());
+    }
+}
