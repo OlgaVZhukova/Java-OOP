c4967481ff946bafc12c8eb12dcca5153404d304
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-04 18:25:23 +0300

    Class Product is realized

---
+++ Homework1/Product.java
@@ -0,0 +1,23 @@
+package Homework1;
+
+public class Product {
+    private String productName;
+    private double price;
+    private int amount;
+    private String unit;
+
+    public Product(String productName, double price, int amount, String unit) {
+        this.productName = productName;
+        this.price = price;
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s\nPrice: %.2f\nAmount: %d\nUnit: %s", this.productName, this.price, this.amount, this.unit);
+    }
+}
