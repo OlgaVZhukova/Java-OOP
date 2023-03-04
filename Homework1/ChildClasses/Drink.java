186b358572ae149bb641e528d6d9176092ad907b
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-04 18:57:18 +0300

    Child Class Drink is added

---
+++ Homework1/ChildClasses/Drink.java
@@ -0,0 +1,16 @@
+package Homework1.ChildClasses;
+
+import Homework1.Product;
+
+public class Drink extends Product {
+    private final double volume;
+    public Drink(String productName, double price, int amount, String unit, double volume) {
+        super(productName, price, amount, unit);
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\nVolume: %s", super.toString(), this.volume);
+    }
+}
