d4eacef5883c7982cee4cbfcd1b091d9a26bae68
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-04 19:50:51 +0300

    Sub Class Pacifier is added

---
+++ Homework1/SubClasses/Pacifier.java
@@ -0,0 +1,14 @@
+package Homework1.SubClasses;
+
+import Homework1.Product;
+
+public class Pacifier extends Product {
+    public Pacifier(String productName, double price, int amount, String unit) {
+        super(productName, price, amount, unit);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
