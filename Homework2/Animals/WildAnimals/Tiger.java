b414decc980f6d301d5ff04ccd58b1bfecf2ffec
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-07 18:47:56 +0300

    Class Tiger is added

---
+++ Homework2/Animals/WildAnimals/Tiger.java
@@ -0,0 +1,7 @@
+package Homework2.Animals.WildAnimals;
+
+public class Tiger implements WildAnimals{
+    public Tiger(int height, double weight, String eyesColor, String habitat, String dateFinding) {
+        super(height, weight, eyesColor, habitat, dateFinding);
+    }
+}
