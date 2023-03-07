47ff720f9caf190f6dde7ad3ca722bfd68770630
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-07 18:34:59 +0300

    Class Dog is added

--- Homework2/Animals/Pets/Dog.java
+++ Homework2/Animals/Pets/Dog.java
@@ -1,10 +1,11 @@
 package Homework2.Animals.Pets;
 
-public class Cat implements Pets{
-    private final boolean woolPresence;
-    this.woolPresence = woolPresence;
+public class Dog implements Pets{
+    private boolean training;
+    public boolean getTraining(){return this.training;}
 
     @Override
     public void showFondling() {
         System.out.println("I am happy dog!!! Auuu!!!");
+    }
 }
