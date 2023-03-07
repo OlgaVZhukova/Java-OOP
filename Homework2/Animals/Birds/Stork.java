ae226d32727f89a6bdbdfec7d0d78761b05cc8bd
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-07 19:08:19 +0300

    Sub-Class Stork is added

---
+++ Homework2/Animals/Birds/Stork.java
@@ -0,0 +1,7 @@
+package Homework2.Animals.Birds;
+
+public class Stork extends Bird {
+    public Stork(int height, double weight, String eyesColor, int flightHeight) {
+        super(height, weight, eyesColor, flightHeight);
+    }
+}
