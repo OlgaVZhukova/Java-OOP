0f5a69de60efa99eb986f9a9bff6409ae5bc69d1
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-07 19:06:50 +0300

    Sub-Class Hen is added

---
+++ Homework2/Animals/Birds/Hen.java
@@ -0,0 +1,7 @@
+package Homework2.Animals.Birds;
+
+public class Hen extends Bird {
+    public Hen(int height, double weight, String eyesColor, int flightHeight) {
+        super(height, weight, eyesColor, flightHeight);
+    }
+}
