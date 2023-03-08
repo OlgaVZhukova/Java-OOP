80f30836b71a830e39be37bf0e73a2a71666470d
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-08 16:32:21 +0300

    Animals file is updated with Properties

--- Homework2/Animals/Animals.java
+++ Homework2/Animals/Animals.java
@@ -1,8 +1,22 @@
 package Homework2.Animals;
 
-public interface Animals {
-    public void makeSound();
-    public void printAnimalInfo();
-    public void showFondling();
-    public void flight();
+import Homework2.Interfaces.PrintAnimalInfo;
+import Homework2.Interfaces.MakeSound;
+
+public abstract class Animals implements MakeSound, PrintAnimalInfo {
+    protected int height;
+    protected double weight;
+    protected String eyesColor;
+
+    protected Animals(int height, double weight, String eyesColor) {
+        this.height = height;
+        this.weight = weight;
+        this.eyesColor = eyesColor;
+    }
+    protected Animals() {
+    }
+    @Override
+    public abstract void makeSound();
+    @Override
+    public abstract void printAnimalInfo();
 }
