13a6f7bfb8001a4ecea6738bb9ad6d83b1fdaead
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-08 13:37:53 +0300

    File with description of User Interface is added

--- Homework2/Main.java
+++ Homework2/Main.java
@@ -1,9 +1,11 @@
 package Homework2;
 
-import Homework2.Animals.Pets.*;
+import Homework2.Zoo.UserInterface;
+
+import java.io.IOException;
 
 public class Main {
-    public static void getAnimalInfo(Pets animal) {
-        System.out.println(animal.toString());
+    public static void main(String[] args) throws IOException {
+        UserInterface.run();
     }
 }
