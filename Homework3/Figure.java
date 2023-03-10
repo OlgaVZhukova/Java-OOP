447c65d86c8958aff74f230fe0d2ac716aeb5ba7
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-10 20:10:17 +0300

    Figure is updated with Orerrided methods

--- Homework3/Figure.java
+++ Homework3/Figure.java
@@ -4,24 +4,15 @@ import Homework3.Interfaces.GetArea;
 import Homework3.Interfaces.GetPerimeter;
 
 public abstract class Figure implements GetArea, GetPerimeter {
-    /*public abstract double getArea();
 
     public String getInfo(Figure obj) {
         String result = String.format("%s\nS: %.2f", obj.getClass().getSimpleName(), obj.getArea());
-        return result;*/
-        public String figureName;
-        public double lengthSide;
-    
-        public Figure(String figureName, double lengthSide) {
-            this.figureName = figureName;
-            this.lengthSide = lengthSide;
-        }
-    
-        public Figure() {
-        }
+        return result;
+    }
     
         @Override
-        public String toString() {
-            return String.format("Фигура: %s\nДлина стороны: %.2f", this.figureName, this.lengthSide);
-        }
+        public abstract double getArea();
+
+        @Override
+        public abstract double getPerimeter();
 }
