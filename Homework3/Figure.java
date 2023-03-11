23428b4d00548fa11070a156f2b6afeeb9c30b3d
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-11 16:20:19 +0300

    File Filgure is updated with comparator

--- Homework3/Figure.java
+++ Homework3/Figure.java
@@ -3,16 +3,26 @@ package Homework3;
 import Homework3.Interfaces.GetArea;
 import Homework3.Interfaces.GetPerimeter;
 
-public abstract class Figure implements GetArea, GetPerimeter {
-
+public abstract class Figure implements Comparable<Figure>, GetArea, GetPerimeter {
+    
     public String getInfo(Figure obj) {
         String result = String.format("%s\nS: %.2f", obj.getClass().getSimpleName(), obj.getArea());
         return result;
     }
     
-        @Override
-        public abstract double getArea();
+        public Figure(int[] is) {
+    }
 
-        @Override
-        public abstract double getPerimeter();
+    @Override
+    public abstract double getArea();
+
+    @Override
+    public abstract double getPerimeter();
+
+    @Override
+    public int compareTo(Figure obj) {
+        if (this.getArea() > obj.getArea()) return 1;
+        else if (this.getArea() < obj.getArea()) return -1;
+        else return 0;
+    }
 }
