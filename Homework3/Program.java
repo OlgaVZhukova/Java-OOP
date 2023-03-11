2354142ff0307e2264739f295eebb27e503169a9
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-11 16:22:59 +0300

    File Program is updated

--- Homework3/Program.java
+++ Homework3/Program.java
@@ -1,9 +1,29 @@
 package Homework3;
 
-//import Homework3.Figure;
+import Homework3.Figures.*;
+//import Homework3
 
 public class Program {
-    public static void getFigureInfo(Figure figure){
-        System.out.println(figure.toString());
+    public static void main(String[] args)
+            throws NegativeValueException, IncorrectTriangleException {
+
+        FiguresArray storage = new FiguresArray();
+
+        Square square = new Square(5);
+        Rectangle rectangle = new Rectangle(5, 7);
+        Triangle triangle = new Triangle(3, 4, 5);
+        Circle circle = new Circle(7);
+        Square sq = new Square();
+
+        storage.add(square);
+        storage.add(rectangle);
+        storage.add(triangle);
+        storage.add(circle);
+        storage.add(sq);
+
+        storage.remove(0);
+        storage.sort();
+        storage.showFigures();
+        storage.showAll();
     }
 }
