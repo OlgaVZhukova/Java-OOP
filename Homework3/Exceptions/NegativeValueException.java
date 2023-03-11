ad1b01b560c07f0d286a1f0fc88a170b744c4a76
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-11 16:23:56 +0300

    Exceptions are added

---
+++ Homework3/Exceptions/NegativeValueException.java
@@ -0,0 +1,23 @@
+package Homework3.Exceptions;
+
+public class NegativeValueException extends Exception {
+    public NegativeValueException() {
+    }
+
+    public NegativeValueException(String message) {
+        super(message);
+    }
+
+    public NegativeValueException(Throwable cause) {
+        super(cause);
+    }
+
+    public NegativeValueException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NegativeValueException(String message, Throwable cause, boolean enableSuppression,
+            boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
