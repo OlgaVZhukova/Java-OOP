f36b106a2d3e1564e1f0813a7b2080f8c625ff40
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-14 20:49:21 +0300

    File Array List is updated with initialization of array

--- Homework4/ArrayList.java
+++ Homework4/ArrayList.java
@@ -1,5 +1,33 @@
 package Homework4;
 
-public class ArrayList {
-    
+public class ArrayList<T> {
+    private static final int startLength = 1;
+    private int length;
+    private T[] data;
+
+    public ArrayList() {
+        this.length = 0;
+        this.data = (T[]) new Object[startLength];
+    }
+
+    public ArrayList(T[] data) {
+        this.length = data.length;
+        this.data = data;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public T[] getData() {
+        return this.data;
+    }
+
+    // TO DO: Печать массива
+
+    // TO DO: Вставка в массив
+
+    // TO DO: Удаление по индексу
+
+    // TO DO: Удаление элемента по значению
 }
