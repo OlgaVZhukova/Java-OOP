33c2bd99e98340e7e232c972091ca409fcb57d3f
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-15 19:26:11 +0300

    Program is updated

--- Homework4/Program.java
+++ Homework4/Program.java
@@ -2,19 +2,32 @@ package Homework4;
 
 public class Program {
     public static void main(String[] args) {
-        // TO DO: добавить работу с цифрами
-
-        // TO DO: добавить элемент
-
-        // TO DO: печать масива
-        // TO DO: удаление элемента по индексу
-        // TO DO: удаление элемента по значению
-        // TO DO: Поиск минимального значения
-        // TO DO: Поиск максимального значения
-        // TO DO: Сумма элементов массива
-        // TO DO: Произведение элементов массива
-        // TO DO: Поиск элемента по индексу
-        // TO DO: Проверка наличия элемента в массиве
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(37); // добавление элемента
+        intList.add(47);
+        intList.add(27);
+        intList.add(55);
+        intList.add(87);
+        intList.add(12);
+        intList.add(51);
+        intList.add(12);
+        intList.add(12);
+        intList.add(12);
+        intList.add(88);
+        intList.add(2);
+        intList.printArray(); // печать масива
+        intList.remForIdx(5); // удаление элемента по индексу
+        intList.remForIdx(5);
+        intList.printArray();
+        intList.removeData(12); // удаление элемента по значению
+        intList.removeData(88);
+        intList.printArray();
+        System.out.println(intList.minData()); // Поиск минимального значения
+        System.out.println(intList.maxData()); // Поиск максимального значения
+        System.out.println(intList.sumData()); // Сумма элементов массива
+        System.out.println(intList.multData()); // Произведение элементов массива
+        System.out.println(intList.findIDX(87)); // Поиск элемента по индексу (если его нет то возвращает -1)
+        System.out.println(intList.findData(874)); // Проверка наличия элемента в массиве. Возвращает true, если элемент в массиве есть, false – нет.
 
         // TO DO: Пузырьковая сортировка
 
