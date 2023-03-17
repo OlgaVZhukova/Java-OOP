a9a95cbde93089d8e4acf2275598b25c621878a5
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-17 19:44:06 +0300

    File Log is added

---
+++ Homework5/Logging/Log.java
@@ -0,0 +1,6 @@
+package Homework5.Logging;
+
+public interface Log {
+    public void writeFile(String expression, String nameFile);
+    public String getExpression(Double x, Double y, Double result, Integer operation);
+}
