ba25020655bea1ef18f7cd76b4c0d866fb13bb83
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-17 20:05:24 +0300

    CommonModel is added

---
+++ Homework5/Models/CommonModel.java
@@ -0,0 +1,21 @@
+package Homework5.Models;
+
+public class CommonModel implements IModel {
+    Double x;
+    Double y;
+
+    @Override
+    public Double result() {
+        return 0.0;
+    }
+
+    @Override
+    public void setX(Double value) {
+        
+    }
+
+    @Override
+    public void setY(Double value) {
+        
+    }
+}
