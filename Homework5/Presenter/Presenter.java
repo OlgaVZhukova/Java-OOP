3b740b78e8e81c92e2a5cca254b599b8fef471fd
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-03-18 18:16:45 +0300

    Presenter is added

---
+++ Homework5/Presenter/Presenter.java
@@ -0,0 +1,66 @@
+package Homework5.Presenter;
+
+import Homework5.Models.*;
+
+import Homework5.Logging.Log;
+import Homework5.Logging.LogTXT;
+
+public class Presenter<T extends CommonModel>  {
+    UI v;
+    T model;
+
+    public Presenter(UI v, T model) {
+        this.model = model;
+        this.v = v;
+    }
+
+    /**
+     * Основной метод, объединяющий получений значений, выбор операции, получение результата и логирование
+     */
+    public void runOperation() {
+        Log log = new LogTXT();
+        String txt = "";
+        Double x = v.inputDouble.apply("Input first number: ");
+        Double y = v.inputDouble.apply("Input second number: ");
+        int choice = v.getChoice();
+        switch (choice) {
+            case 1: {
+                SumModel model = new SumModel();
+                model.setX(x);
+                model.setY(y);
+                Double res = model.result();
+                txt = log.getExpression(x, y, res, choice);
+                System.out.println(res);
+                break;
+            }
+            case 2: {
+                SubtractionModel model = new SubtractionModel();
+                model.setX(x);
+                model.setY(y);
+                Double res = model.result();
+                txt = log.getExpression(x, y, res, choice);
+                System.out.println(res);
+                break;
+            }
+            case 3: {
+                MultiplationModel model = new MultiplationModel();
+                model.setX(x);
+                model.setY(y);
+                Double res = model.result();
+                txt = log.getExpression(x, y, res, choice);
+                System.out.println(res);
+                break;
+            }
+            case 4: {
+                DivisionModel model = new DivisionModel();
+                model.setX(x);
+                model.setY(y);
+                Double res = model.result();
+                txt = log.getExpression(x, y, res, choice);
+                System.out.println(res);
+                break;
+            }
+        }
+        log.writeFile(txt, "Logging\\Saving.txt");
+    }
+}
